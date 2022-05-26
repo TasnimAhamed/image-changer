0e21c4d4f2a1c6910a07d95b96bb8b86383ac78c
TasnimAhmed <tasnim.ahamed.20@gmail.com>
2022-05-26 13:18:09 +0600

    First commit

---
+++ js/main.js
@@ -0,0 +1,99 @@
+
+const slider = document.getElementById('before-after-slider');
+const before = document.getElementById('before-image');
+const beforeImage = before.getElementsByTagName('img')[0];
+const resizer = document.getElementById('resizer');
+
+let active = false;
+
+//Sort overflow out for Overlay Image
+document.addEventListener("DOMContentLoaded", function() {
+  let width = slider.offsetWidth;
+  beforeImage.style.width = width + 'px';
+});
+
+// Adjust width of image on resize 
+window.addEventListener('resize', function() {
+  let width = slider.offsetWidth;
+  beforeImage.style.width = width + 'px';
+})
+
+// resizer.addEventListener('mousedown',function(){
+//   active = true;
+//   resizer.classList.add('resize');
+// });
+
+
+// document.getElementById("before-after-slider").addEventListener('mouseup',function(){
+//   active = false;
+//   resizer.classList.remove('resize');
+// });
+
+
+// document.getElementById("before-after-slider").addEventListener('mouseleave', function() {
+//   active = false;
+//   resizer.classList.remove('resize');
+// });
+
+
+// document.getElementById("before-after-slider").addEventListener('mousemove',function(e){
+    
+//   if (!active) return;
+  
+//   let x = e.pageX;
+//   x -= slider.getBoundingClientRect().left;
+//   slideIt(x);
+//   pauseEvent(e);
+// });
+
+
+// resizer.addEventListener('touchstart',function(){
+//   active = true;
+//   resizer.classList.add('resize');
+// });
+
+
+
+// document.getElementById("before-after-slider").addEventListener('touchend',function(){
+//   active = false;
+//   resizer.classList.remove('resize');
+// });
+
+
+
+// document.getElementById("before-after-slider").addEventListener('touchcancel',function(){
+//   active = false;
+//   resizer.classList.remove('resize'); 
+// });
+
+
+// //calculation for dragging on touch devices
+// document.getElementById("before-after-slider").addEventListener('touchmove',function(e){
+//   if (!active) return;
+//   let x;
+  
+//   let i;
+//   for (i=0; i < e.changedTouches.length; i++) {
+//   x = e.changedTouches[i].pageX; 
+//   }
+  
+//   x -= slider.getBoundingClientRect().left;
+//   slideIt(x);
+//   pauseEvent(e);
+// });
+
+// function slideIt(x){
+//     let transform = Math.max(0,(Math.min(x,slider.offsetWidth)));
+//     before.style.width = transform+"px";
+//     resizer.style.left = transform-0+"px";
+// }
+
+
+// //stop divs being selected.
+// function pauseEvent(e){
+//     if(e.stopPropagation) e.stopPropagation();
+//     if(e.preventDefault) e.preventDefault();
+//     e.cancelBubble=true;
+//     e.returnValue=false;
+//     return false;
+// }
